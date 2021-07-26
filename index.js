76a1b05864dbb770b9299369f0964d717fc33454
Ahmed <achau1019@gmail.com>
2021-07-26 18:34:33 +0000

    added index file that contains module to all utility functions

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue')
+const flatten = flatten('./flatten');
+const head = require('./head');
+const tail = require('./tail');
+const letterPositions = require('./head');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  eqArrays:         eqArrays,
+  eqObjects:        eqObjects,
+  findKey:          findKey,
+  findKeyByValue:   findKeyByValue,
+  flatten:          flatten,
+  head:             head,
+  letterPositions:  letterPositions,
+  map:              map,
+  middle:           middle,
+  tail:             tail,
+  takeUntil:        takeUntil,
+  without:          without
+};
